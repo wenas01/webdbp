7c982e94154c4e8c06712f04c0cc9fcfd69c0310
diazmacarena <macarena.diaz@ucsp.edu.pe>
2025-06-07 09:46:38 -0500

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,13 +1,13 @@
-// Importamos la aplicación Express desde el archivo principal app.js
+// Importamos la aplicación Express desde el archivo principal
 // Esta aplicación contiene todas las rutas, middlewares y configuraciones
-import { app } from '../../src/app.js';
+const { app } = require('../../src/app.js');
 
 // Importamos serverless-http, una biblioteca que permite ejecutar aplicaciones Express 
 // en entornos serverless como AWS Lambda o Netlify Functions
-import serverless from 'serverless-http';
+const serverless = require('serverless-http');
 
 // Exportamos la función handler que envuelve nuestra aplicación Express 
 // para que pueda ser ejecutada como una función serverless
 // Esto permite que nuestra aplicación Express tradicional funcione 
 // en la arquitectura de funciones sin servidor de Netlify
-export const handler = serverless(app);
+exports.handler = serverless(app);
